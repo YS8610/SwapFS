d0a0024797a9aadb76e588922d2ab9a9fcd8c991
YS_workPC <ong_ys86@yahoo.com.sg>
2022-03-05 14:25:38 +0800

    add constants file for cors website and telegram server api

--- swap/src/main/java/tfip/moddb/swap/Constants.java
+++ swap/src/main/java/tfip/moddb/swap/Constants.java
@@ -2,5 +2,7 @@ package tfip.moddb.swap;
 
 
 public class Constants {
-
+    public static final String WEBSITE_CORS = "*";
+    
+    public static final String WEBSITE_TELEGRAM = "https://telegram.arcanetrading.co/camper";
 }
