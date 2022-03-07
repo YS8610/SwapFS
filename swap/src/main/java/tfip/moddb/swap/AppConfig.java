0d435e167eca4a75e981ba87edf2b1d5da332481
YS_workPC <ong_ys86@yahoo.com.sg>
2022-03-07 09:34:08 +0800

    add jwt to secure api endpoint

--- swap/src/main/java/tfip/moddb/swap/AppConfig.java
+++ swap/src/main/java/tfip/moddb/swap/AppConfig.java
@@ -1,25 +1,25 @@
-// package tfip.moddb.swap;
+package tfip.moddb.swap;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.web.servlet.FilterRegistrationBean;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// import tfip.moddb.swap.service.JwtFilter;
+import tfip.moddb.swap.service.JwtFilter;
 
 
-// @Configuration
-// public class AppConfig {
-//     @Autowired
-//     private JwtFilter myFilter;
+@Configuration
+public class AppConfig {
+    @Autowired
+    private JwtFilter myFilter;
 
-//     @Bean
-//     public FilterRegistrationBean<JwtFilter> registerJwtFilter(JwtFilter filter) {
-//         FilterRegistrationBean<JwtFilter> regFilterBean = new FilterRegistrationBean<>();
-//         regFilterBean.setFilter(filter);
-//         regFilterBean.addUrlPatterns("/api/secure/*");
-//         regFilterBean.addUrlPatterns("/api/delete");
-//         regFilterBean.setEnabled(true);
-//         return regFilterBean;
-//     }
-// }
+    @Bean
+    public FilterRegistrationBean<JwtFilter> registerJwtFilter(JwtFilter filter) {
+        FilterRegistrationBean<JwtFilter> regFilterBean = new FilterRegistrationBean<>();
+        regFilterBean.setFilter(filter);
+        regFilterBean.addUrlPatterns("/api/secure/*");
+        regFilterBean.addUrlPatterns("/api/delete");
+        regFilterBean.setEnabled(true);
+        return regFilterBean;
+    }
+}
