946d24015f940106ba094d8049b94a3b7e2eb3b5
YS_workPC <ong_ys86@yahoo.com.sg>
2022-03-20 12:56:56 +0800

    add https to correct pom error and use system properties instead of environment variables

--- swap/src/main/java/tfip/moddb/swap/Constants.java
+++ swap/src/main/java/tfip/moddb/swap/Constants.java
@@ -4,9 +4,9 @@ package tfip.moddb.swap;
 public class Constants {
     public static final String WEBSITE_CORS = "*";
     
-    public static final String WEBSITE_TELEGRAM = "https://telegram.arcanetrading.co/camper";
+    public static final String WEBSITE_TELEGRAM = System.getProperty("WEBSITE_TELEGRAM");
 
-    // public static final String SECRET_KEY = "9fJVnGel72M6NP1H9zr6pY49oOUrdNzG";
-    public static final String SECRET_KEY = System.getenv("JWT_SECRET");
+    public static final String SECRET_KEY = System.getProperty("SECRET_KEY");
+    // set as system property need to use -DWEBSITE_TELEGRAM="API" -DSECRET_KEY="9fJVnGel72M6NP1H9zr6pY49oOUrdNzG"
     
 }
