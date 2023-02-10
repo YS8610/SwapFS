0b805daf47de68c4b7a4deb14e4409f29c9d65b5
YS_workPC <ong_ys86@yahoo.com.sg>
2023-02-10 19:00:46 +0800

    add new function and comment for better understanding of datepicker

--- swapNG/src/app/date-format.ts
+++ swapNG/src/app/date-format.ts
@@ -3,19 +3,33 @@ import { NativeDateAdapter } from "@angular/material/core";
 
 export const AppDateFormats = {
   parse: {
-    dateInput: 'DD.MM.YYYY',
+    dateInput: 'DD/MM/YYYY',
   },
   display: {
-    dateInput: 'DD.MM.YYYY',
+    dateInput: 'DD MM YYYY',
     monthYearLabel: 'MMM YYYY',
     dateA11yLabel: 'LL',
     monthYearA11yLabel: 'MMMM YYYY',
   }
 }
 
+// AppDateFormats object will be passed to CustomDateAdapter class
 @Injectable()
 export class CustomDateAdapter extends NativeDateAdapter {
-  override format(date: Date, displayFormat: any): string {
+  override parse(value: any, parseFormat?:any): Date | null {
+    // parseFormat will get input from AppDateFormats.parse ('DD/MM/YYYY')
+    const valueArray:number[] = value.split("/").map( (x:string) => parseInt(x)||null)
+    if (valueArray.some( x => x===null)) return null
+
+    if (parseFormat === 'DD/MM/YYYY' && valueArray.length==3){
+      console.log(new Date(valueArray[2],valueArray[1],valueArray[0]))
+      return new Date(valueArray[2]>100?valueArray[2]:valueArray[2]+2000,valueArray[1]-1,valueArray[0])
+    }
+    return null
+  }
+
+  override format(date: Date, displayFormat: Object): string {
+    // displayformat will get input from AppDateFormats.display
     const days = date.getDate();
     const months = date.toLocaleString('default', { month: 'short' }); //date.getMonth() + 1;
     const year = date.getFullYear();
